f4c9608c451c59407b9bb6097d2e8c7ea1eead2d
Osama daoud <osamadaoud66@gmail.com>
2024-01-10 07:59:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers , str ){
+    return drivers.filter((element) => element.toLowerCase() ===str.toLowerCase())
+}
+
+function fuzzyMatch(drivers , str){
+    return drivers.filter((element) => element.slice(0,str.length) === str)
+}
+
+function matchName (drivers , str){
+        return drivers.filter((element) => element.name === str)
+}
